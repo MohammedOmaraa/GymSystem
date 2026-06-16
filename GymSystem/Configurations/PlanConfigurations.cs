ccdee21d894fe9fde1d00d8f4290cc2146168b9d
MohammedOmaraa <mohamed.ali.omaraa@gmail.com>
2026-06-16 21:10:10 +0300

    feat: initialize Plans module with EF Core entity and configuration

---
+++ GymSystem/Configurations/PlanConfigurations.cs
@@ -0,0 +1,36 @@
+﻿using GymSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymSystem.Configurations
+{
+    public class PlanConfigurations : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            builder.Property(p => p.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Property(p => p.IsActive)
+                .HasDefaultValue(true);
+
+            builder.ToTable(tb =>
+            {
+                tb.HasCheckConstraint(
+                    "CK_Plan_DurationDays",
+                    "DurationDays BETWEEN 1 AND 365");
+            });
+
+        }
+    }
+}
---
+++ GymSystem/Contexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GymSystem.Configurations;
+using GymSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSystem.Contexts
+{
+    public class GymDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;database=GymSystem;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlanConfigurations());
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
