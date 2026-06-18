dc7af9930f531c1e22f84af0b1a5727109c004d1
MohammedOmaraa <mohamed.ali.omaraa@gmail.com>
2026-06-18 04:11:58 +0300

    feat: add Plans module with index and details views and setup main layout

---
+++ GymSystem/Controllers/PlanController.cs
@@ -0,0 +1,28 @@
+﻿using GymSystem.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSystem.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly GymDbContext _dbContext = new GymDbContext();
+        
+        public async Task<IActionResult> Index()
+        {
+            var plans = await _dbContext.Plans.ToListAsync();
+            
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await _dbContext.Plans.FirstOrDefaultAsync(p => p.Id == id);
+            
+            if (plan == null)
+                return NotFound();
+
+            return View(plan);
+        }
+    }
+}
